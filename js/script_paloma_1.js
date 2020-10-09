ba78c882bae3812505d0d892bc20ac05b204957b
narierao <nariera@uc.cl>
2020-10-09 13:11:28 -0300

    7 Palomas

--- js/script_paloma_1.js
+++ js/script_paloma_1.js
@@ -4,15 +4,18 @@ $(document).ready(function(){
     $("div.id_2#mujer").hide();
     $("div.id_3#mujer").hide();
     $("div.id_4#mujer").hide();
+    $("div.id_5#mujer").hide();
+    $("div.id_6#mujer").hide();
+    $("div.id_7#mujer").hide();
 
     $("img.id_1").mouseover(function(){
         $("img.id_1").fadeTo(1, 0.8); 
         $("img.id_2").fadeTo(1, 0.33);
         $("img.id_3").fadeTo(1, 0.33);
         $("img.id_4").fadeTo(1, 0.33);
-        // $(".id_5").fadeTo(1, 0.33);
-        // $(".id_6").fadeTo(1, 0.33);
-        // $(".id_7").fadeTo(1, 0.33);
+        $("img.id_5").fadeTo(1, 0.33);
+        $("img.id_6").fadeTo(1, 0.33);
+        $("img.id_7").fadeTo(1, 0.33);
         // $(".id_8").fadeTo(1, 0.33);
         // $(".id_9").fadeTo(1, 0.33);
         // $(".id_10").fadeTo(1, 0.33);
@@ -53,9 +56,9 @@ $(document).ready(function(){
         $("img.id_2").fadeTo(1, 1);
         $("img.id_3").fadeTo(1, 1);
         $("img.id_4").fadeTo(1, 1);
-        // $(".id_5").fadeTo(1, 1);
-        // $(".id_6").fadeTo(1, 1);
-        // $(".id_7").fadeTo(1, 1);
+        $("img.id_5").fadeTo(1, 1);
+        $("img.id_6").fadeTo(1, 1);
+        $("img.id_7").fadeTo(1, 1);
         // $(".id_8").fadeTo(1, 1);
         // $(".id_9").fadeTo(1, 1);
         // $(".id_10").fadeTo(1, 1);
@@ -97,9 +100,9 @@ $(document).ready(function(){
         $("img.id_2").fadeTo(1, 0.8);
         $("img.id_3").fadeTo(1, 0.33);
         $("img.id_4").fadeTo(1, 0.33);
-        // $(".id_5").fadeTo(1, 0.33);
-        // $(".id_6").fadeTo(1, 0.33);
-        // $(".id_7").fadeTo(1, 0.33);
+        $("img.id_5").fadeTo(1, 0.33);
+        $("img.id_6").fadeTo(1, 0.33);
+        $("img.id_7").fadeTo(1, 0.33);
         // $(".id_8").fadeTo(1, 0.33);
         // $(".id_9").fadeTo(1, 0.33);
         // $(".id_10").fadeTo(1, 0.33);
@@ -140,9 +143,9 @@ $(document).ready(function(){
         $("img.id_2").fadeTo(1, 1);
         $("img.id_3").fadeTo(1, 1);
         $("img.id_4").fadeTo(1, 1);
-        // $(".id_5").fadeTo(1, 1);
-        // $(".id_6").fadeTo(1, 1);
-        // $(".id_7").fadeTo(1, 1);
+        $("img.id_5").fadeTo(1, 1);
+        $("img.id_6").fadeTo(1, 1);
+        $("img.id_7").fadeTo(1, 1);
         // $(".id_8").fadeTo(1, 1);
         // $(".id_9").fadeTo(1, 1);
         // $(".id_10").fadeTo(1, 1);
@@ -184,9 +187,9 @@ $(document).ready(function(){
         $("img.id_2").fadeTo(1, 0.33);
         $("img.id_3").fadeTo(1, 0.8);
         $("img.id_4").fadeTo(1, 0.33);
-        // $(".id_5").fadeTo(1, 0.33);
-        // $(".id_6").fadeTo(1, 0.33);
-        // $(".id_7").fadeTo(1, 0.33);
+        $("img.id_5").fadeTo(1, 0.33);
+        $("img.id_6").fadeTo(1, 0.33);
+        $("img.id_7").fadeTo(1, 0.33);
         // $(".id_8").fadeTo(1, 0.33);
         // $(".id_9").fadeTo(1, 0.33);
         // $(".id_10").fadeTo(1, 0.33);
@@ -227,9 +230,9 @@ $(document).ready(function(){
         $("img.id_2").fadeTo(1, 1);
         $("img.id_3").fadeTo(1, 1);
         $("img.id_4").fadeTo(1, 1);
-        // $(".id_5").fadeTo(1, 1);
-        // $(".id_6").fadeTo(1, 1);
-        // $(".id_7").fadeTo(1, 1);
+        $("img.id_5").fadeTo(1, 1);
+        $("img.id_6").fadeTo(1, 1);
+        $("img.id_7").fadeTo(1, 1);
         // $(".id_8").fadeTo(1, 1);
         // $(".id_9").fadeTo(1, 1);
         // $(".id_10").fadeTo(1, 1);
@@ -271,9 +274,9 @@ $(document).ready(function(){
         $("img.id_2").fadeTo(1, 0.33);
         $("img.id_3").fadeTo(1, 0.33);
         $("img.id_4").fadeTo(1, 0.8);
-        // $(".id_5").fadeTo(1, 0.33);
-        // $(".id_6").fadeTo(1, 0.33);
-        // $(".id_7").fadeTo(1, 0.33);
+        $("img.id_5").fadeTo(1, 0.33);
+        $("img.id_6").fadeTo(1, 0.33);
+        $("img.id_7").fadeTo(1, 0.33);
         // $(".id_8").fadeTo(1, 0.33);
         // $(".id_9").fadeTo(1, 0.33);
         // $(".id_10").fadeTo(1, 0.33);
@@ -314,9 +317,9 @@ $(document).ready(function(){
         $("img.id_2").fadeTo(1, 1);
         $("img.id_3").fadeTo(1, 1);
         $("img.id_4").fadeTo(1, 1);
-        // $(".id_5").fadeTo(1, 1);
-        // $(".id_6").fadeTo(1, 1);
-        // $(".id_7").fadeTo(1, 1);
+        $("img.id_5").fadeTo(1, 1);
+        $("img.id_6").fadeTo(1, 1);
+        $("img.id_7").fadeTo(1, 1);
         // $(".id_8").fadeTo(1, 1);
         // $(".id_9").fadeTo(1, 1);
         // $(".id_10").fadeTo(1, 1);
@@ -350,6 +353,267 @@ $(document).ready(function(){
         $(".cls-8").css("fill","#998675");
 
         $("div.id_4#mujer").hide()
+
+    });
+
+    $("img.id_5").mouseover(function(){
+        $("img.id_1").fadeTo(1, 0.33); 
+        $("img.id_2").fadeTo(1, 0.33);
+        $("img.id_3").fadeTo(1, 0.33);
+        $("img.id_4").fadeTo(1, 0.33);
+        $("img.id_5").fadeTo(1, 0.8);
+        $("img.id_6").fadeTo(1, 0.33);
+        $("img.id_7").fadeTo(1, 0.33);
+        $("img.id_8").fadeTo(1, 0.33);
+        // $(".id_9").fadeTo(1, 0.33);
+        // $(".id_10").fadeTo(1, 0.33);
+        // $(".id_11").fadeTo(1, 0.33);
+        // $(".id_12").fadeTo(1, 0.33);
+        // $(".id_13").fadeTo(1, 0.33);
+        // $(".id_14").fadeTo(1, 0.33);
+        // $(".id_15").fadeTo(1, 0.33);
+        // $(".id_16").fadeTo(1, 0.33);
+        // $(".id_17").fadeTo(1, 0.33);
+        // $(".id_18").fadeTo(1, 0.33);
+        // $(".id_19").fadeTo(1, 0.33);
+        // $(".id_20").fadeTo(1, 0.33);
+        // $(".id_21").fadeTo(1, 0.33);
+        // $(".id_22").fadeTo(1, 0.33);
+        // $(".id_23").fadeTo(1, 0.33);
+        // $(".id_24").fadeTo(1, 0.33);
+        // $(".id_25").fadeTo(1, 0.33);
+        // $(".id_26").fadeTo(1, 0.33);
+        // $(".id_27").fadeTo(1, 0.33);
+        // $(".id_28").fadeTo(1, 0.33);
+        // $(".id_29").fadeTo(1, 0.33);
+        // $(".id_30").fadeTo(1, 0.33);
+        // $(".id_31").fadeTo(1, 0.33);
+        // $(".id_32").fadeTo(1, 0.33);
+        // $(".id_33").fadeTo(1, 0.33);
+        // $(".id_34").fadeTo(1, 0.33); 
+
+        $(".cls-1").css("fill","#93278F");
+        $(".cls-8").css("fill","#000033");
+
+        $("div.id_5#mujer").show()
+    });
+
+    $("img.id_5").mouseout(function(){
+
+        $("img.id_1").fadeTo(1, 1);
+        $("img.id_2").fadeTo(1, 1);
+        $("img.id_3").fadeTo(1, 1);
+        $("img.id_4").fadeTo(1, 1);
+        $("img.id_5").fadeTo(1, 1);
+        $("img.id_6").fadeTo(1, 1);
+        $("img.id_7").fadeTo(1, 1);
+        // $(".id_8").fadeTo(1, 1);
+        // $(".id_9").fadeTo(1, 1);
+        // $(".id_10").fadeTo(1, 1);
+        // $(".id_11").fadeTo(1, 1);
+        // $(".id_12").fadeTo(1, 1);
+        // $(".id_13").fadeTo(1, 1);
+        // $(".id_14").fadeTo(1, 1);
+        // $(".id_15").fadeTo(1, 1);
+        // $(".id_16").fadeTo(1, 1);
+        // $(".id_17").fadeTo(1, 1);
+        // $(".id_18").fadeTo(1, 1);
+        // $(".id_19").fadeTo(1, 1);
+        // $(".id_20").fadeTo(1, 1);
+        // $(".id_21").fadeTo(1, 1);
+        // $(".id_22").fadeTo(1, 1);
+        // $(".id_23").fadeTo(1, 1);
+        // $(".id_24").fadeTo(1, 1);
+        // $(".id_25").fadeTo(1, 1);
+        // $(".id_26").fadeTo(1, 1);
+        // $(".id_27").fadeTo(1, 1);
+        // $(".id_28").fadeTo(1, 1);
+        // $(".id_29").fadeTo(1, 1);
+        // $(".id_30").fadeTo(1, 1);
+        // $(".id_31").fadeTo(1, 1);
+        // $(".id_32").fadeTo(1, 1);
+        // $(".id_33").fadeTo(1, 1);
+        // $(".id_34").fadeTo(1, 1);
+
+        // vuelvo a la normalidad los backgrounds
+        $(".cls-1").css("fill","#FFEEEB");
+        $(".cls-8").css("fill","#998675");
+
+        $("div.id_5#mujer").hide()
+
+    });
+
+    $("img.id_6").mouseover(function(){
+        $("img.id_1").fadeTo(1, 0.33); 
+        $("img.id_2").fadeTo(1, 0.33);
+        $("img.id_3").fadeTo(1, 0.33);
+        $("img.id_4").fadeTo(1, 0.33);
+        $("img.id_5").fadeTo(1, 0.33);
+        $("img.id_6").fadeTo(1, 0.8);
+        $("img.id_7").fadeTo(1, 0.33);
+        // $(".id_8").fadeTo(1, 0.33);
+        // $(".id_9").fadeTo(1, 0.33);
+        // $(".id_10").fadeTo(1, 0.33);
+        // $(".id_11").fadeTo(1, 0.33);
+        // $(".id_12").fadeTo(1, 0.33);
+        // $(".id_13").fadeTo(1, 0.33);
+        // $(".id_14").fadeTo(1, 0.33);
+        // $(".id_15").fadeTo(1, 0.33);
+        // $(".id_16").fadeTo(1, 0.33);
+        // $(".id_17").fadeTo(1, 0.33);
+        // $(".id_18").fadeTo(1, 0.33);
+        // $(".id_19").fadeTo(1, 0.33);
+        // $(".id_20").fadeTo(1, 0.33);
+        // $(".id_21").fadeTo(1, 0.33);
+        // $(".id_22").fadeTo(1, 0.33);
+        // $(".id_23").fadeTo(1, 0.33);
+        // $(".id_24").fadeTo(1, 0.33);
+        // $(".id_25").fadeTo(1, 0.33);
+        // $(".id_26").fadeTo(1, 0.33);
+        // $(".id_27").fadeTo(1, 0.33);
+        // $(".id_28").fadeTo(1, 0.33);
+        // $(".id_29").fadeTo(1, 0.33);
+        // $(".id_30").fadeTo(1, 0.33);
+        // $(".id_31").fadeTo(1, 0.33);
+        // $(".id_32").fadeTo(1, 0.33);
+        // $(".id_33").fadeTo(1, 0.33);
+        // $(".id_34").fadeTo(1, 0.33); 
+
+        $(".cls-1").css("fill","#93278F");
+        $(".cls-8").css("fill","#000033");
+
+        $("div.id_6#mujer").show()
+    });
+
+    $("img.id_6").mouseout(function(){
+
+        $("img.id_1").fadeTo(1, 1);
+        $("img.id_2").fadeTo(1, 1);
+        $("img.id_3").fadeTo(1, 1);
+        $("img.id_4").fadeTo(1, 1);
+        $("img.id_5").fadeTo(1, 1);
+        $("img.id_6").fadeTo(1, 1);
+        $("img.id_7").fadeTo(1, 1);
+        // $(".id_8").fadeTo(1, 1);
+        // $(".id_9").fadeTo(1, 1);
+        // $(".id_10").fadeTo(1, 1);
+        // $(".id_11").fadeTo(1, 1);
+        // $(".id_12").fadeTo(1, 1);
+        // $(".id_13").fadeTo(1, 1);
+        // $(".id_14").fadeTo(1, 1);
+        // $(".id_15").fadeTo(1, 1);
+        // $(".id_16").fadeTo(1, 1);
+        // $(".id_17").fadeTo(1, 1);
+        // $(".id_18").fadeTo(1, 1);
+        // $(".id_19").fadeTo(1, 1);
+        // $(".id_20").fadeTo(1, 1);
+        // $(".id_21").fadeTo(1, 1);
+        // $(".id_22").fadeTo(1, 1);
+        // $(".id_23").fadeTo(1, 1);
+        // $(".id_24").fadeTo(1, 1);
+        // $(".id_25").fadeTo(1, 1);
+        // $(".id_26").fadeTo(1, 1);
+        // $(".id_27").fadeTo(1, 1);
+        // $(".id_28").fadeTo(1, 1);
+        // $(".id_29").fadeTo(1, 1);
+        // $(".id_30").fadeTo(1, 1);
+        // $(".id_31").fadeTo(1, 1);
+        // $(".id_32").fadeTo(1, 1);
+        // $(".id_33").fadeTo(1, 1);
+        // $(".id_34").fadeTo(1, 1);
+
+        // vuelvo a la normalidad los backgrounds
+        $(".cls-1").css("fill","#FFEEEB");
+        $(".cls-8").css("fill","#998675");
+
+        $("div.id_6#mujer").hide()
+
+    });
+
+    $("img.id_7").mouseover(function(){
+        $("img.id_1").fadeTo(1, 0.33); 
+        $("img.id_2").fadeTo(1, 0.33);
+        $("img.id_3").fadeTo(1, 0.33);
+        $("img.id_4").fadeTo(1, 0.33);
+        $("img.id_5").fadeTo(1, 0.33);
+        $("img.id_6").fadeTo(1, 0.33);
+        $("img.id_7").fadeTo(1, 0.8);
+        $("img.id_8").fadeTo(1, 0.33);
+        // $(".id_9").fadeTo(1, 0.33);
+        // $(".id_10").fadeTo(1, 0.33);
+        // $(".id_11").fadeTo(1, 0.33);
+        // $(".id_12").fadeTo(1, 0.33);
+        // $(".id_13").fadeTo(1, 0.33);
+        // $(".id_14").fadeTo(1, 0.33);
+        // $(".id_15").fadeTo(1, 0.33);
+        // $(".id_16").fadeTo(1, 0.33);
+        // $(".id_17").fadeTo(1, 0.33);
+        // $(".id_18").fadeTo(1, 0.33);
+        // $(".id_19").fadeTo(1, 0.33);
+        // $(".id_20").fadeTo(1, 0.33);
+        // $(".id_21").fadeTo(1, 0.33);
+        // $(".id_22").fadeTo(1, 0.33);
+        // $(".id_23").fadeTo(1, 0.33);
+        // $(".id_24").fadeTo(1, 0.33);
+        // $(".id_25").fadeTo(1, 0.33);
+        // $(".id_26").fadeTo(1, 0.33);
+        // $(".id_27").fadeTo(1, 0.33);
+        // $(".id_28").fadeTo(1, 0.33);
+        // $(".id_29").fadeTo(1, 0.33);
+        // $(".id_30").fadeTo(1, 0.33);
+        // $(".id_31").fadeTo(1, 0.33);
+        // $(".id_32").fadeTo(1, 0.33);
+        // $(".id_33").fadeTo(1, 0.33);
+        // $(".id_34").fadeTo(1, 0.33); 
+
+        $(".cls-1").css("fill","#93278F");
+        $(".cls-8").css("fill","#000033");
+
+        $("div.id_7#mujer").show()
+    });
+
+    $("img.id_7").mouseout(function(){
+
+        $("img.id_1").fadeTo(1, 1);
+        $("img.id_2").fadeTo(1, 1);
+        $("img.id_3").fadeTo(1, 1);
+        $("img.id_4").fadeTo(1, 1);
+        $("img.id_5").fadeTo(1, 1);
+        $("img.id_6").fadeTo(1, 1);
+        $("img.id_7").fadeTo(1, 1);
+        $("img.id_8").fadeTo(1, 1);
+        // $(".id_9").fadeTo(1, 1);
+        // $(".id_10").fadeTo(1, 1);
+        // $(".id_11").fadeTo(1, 1);
+        // $(".id_12").fadeTo(1, 1);
+        // $(".id_13").fadeTo(1, 1);
+        // $(".id_14").fadeTo(1, 1);
+        // $(".id_15").fadeTo(1, 1);
+        // $(".id_16").fadeTo(1, 1);
+        // $(".id_17").fadeTo(1, 1);
+        // $(".id_18").fadeTo(1, 1);
+        // $(".id_19").fadeTo(1, 1);
+        // $(".id_20").fadeTo(1, 1);
+        // $(".id_21").fadeTo(1, 1);
+        // $(".id_22").fadeTo(1, 1);
+        // $(".id_23").fadeTo(1, 1);
+        // $(".id_24").fadeTo(1, 1);
+        // $(".id_25").fadeTo(1, 1);
+        // $(".id_26").fadeTo(1, 1);
+        // $(".id_27").fadeTo(1, 1);
+        // $(".id_28").fadeTo(1, 1);
+        // $(".id_29").fadeTo(1, 1);
+        // $(".id_30").fadeTo(1, 1);
+        // $(".id_31").fadeTo(1, 1);
+        // $(".id_32").fadeTo(1, 1);
+        // $(".id_33").fadeTo(1, 1);
+        // $(".id_34").fadeTo(1, 1);
+
+        // vuelvo a la normalidad los backgrounds
+        $(".cls-1").css("fill","#FFEEEB");
+        $(".cls-8").css("fill","#998675");
+
+        $("div.id_7#mujer").hide()
 
     });
   });
