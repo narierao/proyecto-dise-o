f797aa9b84f340eae391cf9b048db1aaa0b2415f
narierao <nariera@uc.cl>
2020-10-09 13:37:49 -0300

    8 Palomas

--- js/script_paloma_1.js
+++ js/script_paloma_1.js
@@ -733,5 +733,178 @@ $(document).ready(function(){
 
     });
 
+    $("img.id_9").mouseover(function(){
+        $("img.id_1").fadeTo(1, 0.33); 
+        $("img.id_2").fadeTo(1, 0.33);
+        $("img.id_3").fadeTo(1, 0.33);
+        $("img.id_4").fadeTo(1, 0.33);
+        $("img.id_5").fadeTo(1, 0.33);
+        $("img.id_6").fadeTo(1, 0.33);
+        $("img.id_7").fadeTo(1, 0.33);
+        $("img.id_8").fadeTo(1, 0.33);
+        $("img.id_9").fadeTo(1, 0.8);
+        $("img.id_10").fadeTo(1, 0.33);
+        $("img.id_11").fadeTo(1, 0.33);
+        $("img.id_12").fadeTo(1, 0.33);
+        $("img.id_13").fadeTo(1, 0.33);
+        $("img.id_14").fadeTo(1, 0.33);
+        $("img.id_15").fadeTo(1, 0.33);
+        $("img.id_16").fadeTo(1, 0.33);
+        $("img.id_17").fadeTo(1, 0.33);
+        $("img.id_18").fadeTo(1, 0.33);
+        $("img.id_19").fadeTo(1, 0.33);
+        $("img.id_20").fadeTo(1, 0.33);
+        $("img.id_21").fadeTo(1, 0.33);
+        $("img.id_22").fadeTo(1, 0.33);
+        $("img.id_23").fadeTo(1, 0.33);
+        $("img.id_24").fadeTo(1, 0.33);
+        $("img.id_25").fadeTo(1, 0.33);
+        $("img.id_26").fadeTo(1, 0.33);
+        $("img.id_27").fadeTo(1, 0.33);
+        $("img.id_28").fadeTo(1, 0.33);
+        $("img.id_29").fadeTo(1, 0.33);
+        $("img.id_30").fadeTo(1, 0.33);
+        $("img.id_31").fadeTo(1, 0.33);
+        $("img.id_32").fadeTo(1, 0.33);
+        $("img.id_33").fadeTo(1, 0.33);
+        $("img.id_34").fadeTo(1, 0.33); 
+
+        $(".cls-1").css("fill","#93278F");
+        $(".cls-8").css("fill","#000033");
+
+        $("div.id_9#mujer").show()
+    });
+
+    $("img.id_9").mouseout(function(){
+
+        $("img.id_1").fadeTo(1, 1);
+        $("img.id_2").fadeTo(1, 1);
+        $("img.id_3").fadeTo(1, 1);
+        $("img.id_4").fadeTo(1, 1);
+        $("img.id_5").fadeTo(1, 1);
+        $("img.id_6").fadeTo(1, 1);
+        $("img.id_7").fadeTo(1, 1);
+        $("img.id_8").fadeTo(1, 1);
+        $("img.id_9").fadeTo(1, 1);
+        $("img.id_10").fadeTo(1, 1);
+        $("img.id_11").fadeTo(1, 1);
+        $("img.id_12").fadeTo(1, 1);
+        $("img.id_13").fadeTo(1, 1);
+        $("img.id_14").fadeTo(1, 1);
+        $("img.id_15").fadeTo(1, 1);
+        $("img.id_16").fadeTo(1, 1);
+        $("img.id_17").fadeTo(1, 1);
+        $("img.id_18").fadeTo(1, 1);
+        $("img.id_19").fadeTo(1, 1);
+        $("img.id_20").fadeTo(1, 1);
+        $("img.id_21").fadeTo(1, 1);
+        $("img.id_22").fadeTo(1, 1);
+        $("img.id_23").fadeTo(1, 1);
+        $("img.id_24").fadeTo(1, 1);
+        $("img.id_25").fadeTo(1, 1);
+        $("img.id_26").fadeTo(1, 1);
+        $("img.id_27").fadeTo(1, 1);
+        $("img.id_28").fadeTo(1, 1);
+        $("img.id_29").fadeTo(1, 1);
+        $("img.id_30").fadeTo(1, 1);
+        $("img.id_31").fadeTo(1, 1);
+        $("img.id_32").fadeTo(1, 1);
+        $("img.id_33").fadeTo(1, 1);
+        $("img.id_34").fadeTo(1, 1);
+
+        // vuelvo a la normalidad los backgrounds
+        $(".cls-1").css("fill","#FFEEEB");
+        $(".cls-8").css("fill","#998675");
+
+        $("div.id_9#mujer").hide()
+
+    });
+
+    $("img.id_10").mouseover(function(){
+        $("img.id_1").fadeTo(1, 0.33); 
+        $("img.id_2").fadeTo(1, 0.33);
+        $("img.id_3").fadeTo(1, 0.33);
+        $("img.id_4").fadeTo(1, 0.33);
+        $("img.id_5").fadeTo(1, 0.33);
+        $("img.id_6").fadeTo(1, 0.33);
+        $("img.id_7").fadeTo(1, 0.33);
+        $("img.id_8").fadeTo(1, 0.33);
+        $("img.id_9").fadeTo(1, 0.33);
+        $("img.id_10").fadeTo(1, 0.8);
+        $("img.id_11").fadeTo(1, 0.33);
+        $("img.id_12").fadeTo(1, 0.33);
+        $("img.id_13").fadeTo(1, 0.33);
+        $("img.id_14").fadeTo(1, 0.33);
+        $("img.id_15").fadeTo(1, 0.33);
+        $("img.id_16").fadeTo(1, 0.33);
+        $("img.id_17").fadeTo(1, 0.33);
+        $("img.id_18").fadeTo(1, 0.33);
+        $("img.id_19").fadeTo(1, 0.33);
+        $("img.id_20").fadeTo(1, 0.33);
+        $("img.id_21").fadeTo(1, 0.33);
+        $("img.id_22").fadeTo(1, 0.33);
+        $("img.id_23").fadeTo(1, 0.33);
+        $("img.id_24").fadeTo(1, 0.33);
+        $("img.id_25").fadeTo(1, 0.33);
+        $("img.id_26").fadeTo(1, 0.33);
+        $("img.id_27").fadeTo(1, 0.33);
+        $("img.id_28").fadeTo(1, 0.33);
+        $("img.id_29").fadeTo(1, 0.33);
+        $("img.id_30").fadeTo(1, 0.33);
+        $("img.id_31").fadeTo(1, 0.33);
+        $("img.id_32").fadeTo(1, 0.33);
+        $("img.id_33").fadeTo(1, 0.33);
+        $("img.id_34").fadeTo(1, 0.33); 
+
+        $(".cls-1").css("fill","#93278F");
+        $(".cls-8").css("fill","#000033");
+
+        $("div.id_10#mujer").show()
+    });
+
+    $("img.id_10").mouseout(function(){
+
+        $("img.id_1").fadeTo(1, 1);
+        $("img.id_2").fadeTo(1, 1);
+        $("img.id_3").fadeTo(1, 1);
+        $("img.id_4").fadeTo(1, 1);
+        $("img.id_5").fadeTo(1, 1);
+        $("img.id_6").fadeTo(1, 1);
+        $("img.id_7").fadeTo(1, 1);
+        $("img.id_8").fadeTo(1, 1);
+        $("img.id_9").fadeTo(1, 1);
+        $("img.id_10").fadeTo(1, 1);
+        $("img.id_11").fadeTo(1, 1);
+        $("img.id_12").fadeTo(1, 1);
+        $("img.id_13").fadeTo(1, 1);
+        $("img.id_14").fadeTo(1, 1);
+        $("img.id_15").fadeTo(1, 1);
+        $("img.id_16").fadeTo(1, 1);
+        $("img.id_17").fadeTo(1, 1);
+        $("img.id_18").fadeTo(1, 1);
+        $("img.id_19").fadeTo(1, 1);
+        $("img.id_20").fadeTo(1, 1);
+        $("img.id_21").fadeTo(1, 1);
+        $("img.id_22").fadeTo(1, 1);
+        $("img.id_23").fadeTo(1, 1);
+        $("img.id_24").fadeTo(1, 1);
+        $("img.id_25").fadeTo(1, 1);
+        $("img.id_26").fadeTo(1, 1);
+        $("img.id_27").fadeTo(1, 1);
+        $("img.id_28").fadeTo(1, 1);
+        $("img.id_29").fadeTo(1, 1);
+        $("img.id_30").fadeTo(1, 1);
+        $("img.id_31").fadeTo(1, 1);
+        $("img.id_32").fadeTo(1, 1);
+        $("img.id_33").fadeTo(1, 1);
+        $("img.id_34").fadeTo(1, 1);
+
+        // vuelvo a la normalidad los backgrounds
+        $(".cls-1").css("fill","#FFEEEB");
+        $(".cls-8").css("fill","#998675");
+
+        $("div.id_10#mujer").hide()
+
+    });
 
   });
